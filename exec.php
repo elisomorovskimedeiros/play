c0e840b3f88ecefa0a2f28189b4354f5fbeb370a
elisomorovskimedeiros <eli.sm@hotmail.com>
2018-02-05 18:53:18 -0200

    Commit após inicio do GUI

--- exec.php
+++ exec.php
@@ -1,7 +1,32 @@
 <?php
 
-require_once ("teste.php");
+/*
+require_once ("controller/teste.php");
 
 $t = new mostra;
 $t->msg();
+?>*/
+define('BASE_PATH', __DIR__ . '/');
+
+//include(BASE_PATH . 'controller/conexao.php');
+//include(BASE_PATH . 'model/cliente.php');
+//set_include_path(__DIR__);
+include (__DIR__ . '/controller/conexao.php');
+include (__DIR__ . '/gui/pagPrincipal.php');
+//include("cliente.php");
+
+
+//$con = new Conexao();
+//$con->pesquisarTodosBrinquedos();
+
+
+
+
+//$con->inserirIdCliente($evento_);
+
+
+
+//print_r($con->verificarDisponibilidade(1));
+
 ?>
+
